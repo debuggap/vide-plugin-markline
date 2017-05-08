616d8064e8dd5cb3b9193a0ba849348fda603f23
vic wang <wangluofei596@lu.com>
2017-05-08 23:21:55 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,93 @@
+import './assets/index.scss'
+export default {
+  props: ['addItem'],
+  name: 'linemark',
+  data () {
+    return {
+      bgColors: {
+        '#fbbc05': 0,
+        '#A6E22E': 0,
+        '#66D9EF': 0,
+        '#AE81FF': 0,
+        '#F92672': 0,
+        '#e54724': 0
+      },
+      colorCache: {},
+      items: [],
+      showLineMark: true
+    }
+  },
+  methods: {
+    getColor (item) {
+      let colorIndex = item.path + ':' + item.row
+      if (this.colorCache[colorIndex]) {
+        return this.colorCache[colorIndex]
+      } else {
+        let keys = Object.keys(this.bgColors)
+        let key = null
+        keys.some((k) => {
+          if (!this.bgColors[k]) {
+            key = k
+            return true
+          }
+        })
+        this.colorCache[colorIndex] = key
+        this.bgColors[key] = 1
+        return key
+      }
+    },
+    cleanColor (index) {
+      let item = this.items[index]
+      let key = this.getColor(item)
+      let colorIndex = item.path + ':' + item.row
+      delete this.colorCache[colorIndex]
+      this.bgColors[key] = 0
+    },
+    activeLine () {
+      this.addItem()
+    },
+    pushItem (obj) {
+      if (this.items.length === 6) {
+        return
+      }
+      let exists = this.items.some((item) => {
+        if (item.path === obj.path && item.row === obj.row) {
+          return true
+        }
+      })
+      if (!exists) {
+        this.items.push(obj)
+      }
+    },
+    deleteItem (index) {
+      this.cleanColor(index)
+      this.items.splice(index, 1)
+    },
+    close (e) {
+      let len = this.items.length
+      for (let i = 0; i < len; i++) {
+        this.cleanColor(i)
+      }
+      this.items = []
+      this.showLineMark = false
+      e.stopPropagation()
+    }
+  },
+  render (h) {
+    let lists = this.items.map((item, index) => {
+      let color = this.getColor(item)
+      return <li>
+        <div style={{'background-color': color}}>{item.content}</div>
+        <span class="btn-close" onClick={() => this.deleteItem(index)} style={{'background-color': color}}></span>
+      </li>
+    })
+    return (
+      <div class="linemark" style={{display: this.showLineMark ? 'block' : 'none'}}>
+        <div class="linemark-button btn-UI" onClick={this.activeLine}>Mark Active Line<span onClick={this.close} class="btn-close"></span></div>
+        <ul>
+          {lists}
+        </ul>
+      </div>
+    )
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,55 @@
+import App from './App.js'
+import path from 'path'
+
+export default ({editor, store, view, packageInfo, baseClass}) => {
+  store.dispatch('extmenu/addItem', {
+    type: 'editorContext',
+    name: 'Mark Active Line',
+    func: 'videPluginMarkline:addItem'
+  })
+  // return execute class
+  return class videPluginMarkline extends baseClass {
+    constructor () {
+      super()
+      let self = this
+      let props = {
+        propsData: {
+          addItem: function () {
+            let item = self.getCurrentLineInfo()
+            item && this.pushItem(item)
+          }
+        }
+      }
+      let stylePath = path.join(packageInfo.path, './dist/index.css')
+      this.$mount({app: App, props, stylePath})
+    }
+    addItem () {
+      this.vm.showLineMark = true
+      let item = this.getCurrentLineInfo()
+      item && this.vm.pushItem(item)
+    }
+    getCurrentLineInfo () {
+      let result
+      try {
+        let filepath = store.state.editor.currentFile
+        if (!filepath) {
+          return null
+        }
+        let row = editor.getCursorPosition().row
+        let content = editor.session.getLine(row).slice(0, 20)
+        // content = htmlspecialchars(content)
+        result = {
+          content,
+          path: filepath,
+          row
+        }
+      } catch (e) {
+        result = null
+      }
+      return result
+    }
+    $clean () {
+      store.dispatch('extmenu/removeItem', {name: 'Mark Active Line'})
+    }
+  }
+}
